d168609084851466bef744231d051d3299941522
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2020-07-03 08:24:26 +0200

    module conversion

---
+++ M365Monitoring.psm1
@@ -0,0 +1,9 @@
+﻿foreach ($file in (Get-ChildItem -Path "$PSScriptRoot\internal\functions"))
+{
+	. $file.FullName
+}
+
+foreach ($file in (Get-ChildItem -Path "$PSScriptRoot\functions"))
+{
+	. $file.FullName
+}
