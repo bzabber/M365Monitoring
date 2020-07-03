da6dc1fcdbf076c1c1b5e354d6dc866a4cd47ca0
Bryan Zabchuk <bryan.zabchuk@microsoft.com>
2020-07-03 16:59:54 -0400

    Corrected issue with weird hash literal and unexpected token.

--- functions/Get-M365ServiceHealth.ps1
+++ functions/Get-M365ServiceHealth.ps1
@@ -74,11 +74,11 @@
 					O365WorkloadId          = $O365Workload.Id
 					O365WorkloadDisplayName = $O365Workload.WorkloadDisplayName
 					O365WorkloadStatus      = $O365Workload.Status
-					0365WorkloadIncidentID   = $O365Workload.IncidentIds
+					O365WorkloadIncidentID  = $O365Workload.IncidentIds
 					O365FeatureName         = $O365Feature.FeatureDisplayName
 					O365FeatureStatus       = $O365Feature.FeatureStatus
 					O365ImpactDesc          = $0365Feature.ImpactDescription
-				}]
+				}
 				
 			}
 		}
