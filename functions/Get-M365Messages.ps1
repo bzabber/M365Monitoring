84b0dd5ffb89871b9a914a1693df9b13306b4081
Bryan Zabchuk <bryan.zabchuk@microsoft.com>
2020-10-16 13:30:22 -0400

    Fixed PostIncidentDocumentURL

--- functions/Get-M365Messages.ps1
+++ functions/Get-M365Messages.ps1
@@ -96,7 +96,7 @@
 					O365MsgUpdatedTime                  = $FeatureMessage.LastUpdatedTime
 					O365MsgHistPubTime                  = $MessageHistory.PublishedTime
 					O365MessageHistText                 = $MessageHistory.MessageText
-					O365MsgPostIncidentURL              = $Message.PostIncidentDocumentUrl
+					O365MsgPostIncidentURL              = $FeatureMessage.PostIncidentDocumentUrl
 					O365MsgAffectedWorkloadDisplayNames = $FeatureMessage.AffectedWorkloadDisplayNames -join ","
 				}
 			}
